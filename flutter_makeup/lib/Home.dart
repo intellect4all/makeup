ea3b016d6cb1e81bebf3f1e5e9f30674a562a6c2
Mbaiorga <gabrielmbaiorga@gmail.com>
2021-04-22 12:35:34 +0100

    added project asset

---
+++ flutter_makeup/lib/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
